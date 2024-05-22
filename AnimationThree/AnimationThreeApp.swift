c78fcbf583e2d9c35e235552cfb157603f52a07a
Abdusamad <132121525+AbdusamadIOS@users.noreply.github.com>
2024-05-22 21:43:31 +0500

    Initial Commit

---
+++ AnimationThree/AnimationThreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationThreeApp.swift
+//  AnimationThree
+//
+//  Created by Abdusamad Mamasoliyev on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationThreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
